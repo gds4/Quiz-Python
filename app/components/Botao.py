48497b7e0ed2899eab1828f8b0735cb8d3d2c858
gds4 <gdantas536@gmail.com>
2025-02-04 14:08:37 -0300

    Adicao de comportamento em botao de menu

--- app/components/Botao.py
+++ app/components/Botao.py
@@ -1,25 +1,40 @@
 import pygame
 
 class Botao:
-    def __init__(self, x, y, largura, altura, cor_botao, fonte, texto, cor_texto):
+    def __init__(self, x, y, largura, altura, cor_botao_original, cor_botao_hover, fonte, texto, cor_texto):
         self.x = x
         self.y = y
         self.largura = largura
         self.altura = altura
+
+        self.cor_botao_original = cor_botao_original 
+        self.cor_botao_hover = cor_botao_hover
+        self.cor_atual = cor_botao_original
+
         self.fonte = fonte
         self.texto = texto
         self.cor_texto = cor_texto
-        self.cor_botao = cor_botao 
         self.texto_renderizado = self.fonte.render(self.texto, True, cor_texto)
         self.texto_rect = self.texto_renderizado.get_rect(center=(self.x + self.largura // 2, self.y + self.altura // 2))
 
     def desenhar(self, tela):
         
-        pygame.draw.rect(tela, self.cor_botao, (self.x, self.y, self.largura, self.altura), border_radius=15)
+        pygame.draw.rect(tela, self.cor_atual, (self.x, self.y, self.largura, self.altura), border_radius=15)
         
         tela.blit(self.texto_renderizado, self.texto_rect)
+
+    def mouse_em_cima(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        return self.x <= mouse_x <= self.x + self.largura and self.y <= mouse_y <= self.y + self.altura
+
+    def atualizar(self):
+        if self.mouse_em_cima():
+            self.cor_atual = self.cor_botao_hover
+        else:
+            self.cor_atual = self.cor_botao_original
+
 
     def verificar_click(self, posicao):
         if (self.x <= posicao[0] <= self.x + self.largura and
             self.y <= posicao[1] <= self.y + self.altura):
-            print("Botão pressionado!")
+            return True
--- app/states/menu.py
+++ app/states/menu.py
@@ -1,31 +1,42 @@
 import pygame
 import sys
 from components.Botao import Botao 
-from config import BRANCO, PRETO, LARGURA_TELA, ALTURA_TELA
+from config import BRANCO, PRETO, CINZA, VERMELHO, VERMELHO_CLARO, LARGURA_TELA, ALTURA_TELA
 
 class Menu:
     def __init__(self, game):
         self.game = game  # Referência ao jogo principal
         self.fonte = pygame.font.Font(None, 48)
-        self.botaoJogar = Botao(x=80, y=250, largura=200, altura=50, cor_botao=BRANCO, fonte=self.fonte, texto='Jogar', cor_texto = PRETO)
-        self.botaoSair = Botao(x=80, y=320, largura=200, altura=50, cor_botao=BRANCO, fonte=self.fonte, texto='Sair', cor_texto = PRETO)
+        self.botaoJogar = Botao(x=80, y=450, largura=200, altura=50, cor_botao_original = BRANCO, cor_botao_hover = CINZA, fonte=self.fonte, texto='Jogar', cor_texto = PRETO)
+        self.botaoSair = Botao(x=80, y=520, largura=200, altura=50, cor_botao_original = VERMELHO,cor_botao_hover = VERMELHO_CLARO, fonte=self.fonte, texto='Sair', cor_texto = PRETO)
         self.imagem_fundo = pygame.image.load('app/assets/gradiente_verde_azul.png')
         self.imagem_fundo = pygame.transform.scale(self.imagem_fundo, (LARGURA_TELA, ALTURA_TELA))
+        self.quiz_image = pygame.image.load('app/assets/quiz.png')
+        self.quiz_image = pygame.transform.scale(self.quiz_image, (300, 300))
+
+        self.cursor_imagem = pygame.image.load("app/assets/cursor.png")
+        self.cursor_imagem = pygame.transform.scale(self.cursor_imagem, (32, 32))
+        pygame.mouse.set_visible(False) 
 
     def capturar_eventos(self):
         """Captura eventos do teclado."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.game.executando = False
-                pygame.quit()
-                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if self.botaoSair.verificar_click(pygame.mouse.get_pos()):
+                    self.game.executando = False
     def atualizar(self):
-        """Atualiza a lógica do menu (se necessário)."""
+        self.botaoJogar.atualizar()
+        self.botaoSair.atualizar()
         pass
 
     def desenhar(self):
         #Desenha o menu na tela
         self.game.tela.blit(self.imagem_fundo,(0,0))
+        self.game.tela.blit(self.quiz_image, (30,100))
         self.botaoJogar.desenhar(self.game.tela)
         self.botaoSair.desenhar(self.game.tela)
+        x, y = pygame.mouse.get_pos()
+        self.game.tela.blit(self.cursor_imagem, (x, y))
         
