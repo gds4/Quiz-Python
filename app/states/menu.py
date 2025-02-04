9a9bb76760bfc108952323d37968f1659cc0d660
gds4 <gdantas536@gmail.com>
2025-02-04 11:45:46 -0300

    Criacao dos botoes e plano de fundo

--- app/states/menu.py
+++ app/states/menu.py
@@ -1,13 +1,16 @@
 import pygame
 import sys
 from components.Botao import Botao 
-from config import BRANCO, PRETO
+from config import BRANCO, PRETO, LARGURA_TELA, ALTURA_TELA
 
 class Menu:
     def __init__(self, game):
         self.game = game  # Referência ao jogo principal
         self.fonte = pygame.font.Font(None, 48)
-        self.botao = Botao(x=300, y=250, largura=200, altura=50, cor_botao=BRANCO, fonte=self.fonte, texto='Clique Aqui', cor_texto = PRETO)
+        self.botaoJogar = Botao(x=80, y=250, largura=200, altura=50, cor_botao=BRANCO, fonte=self.fonte, texto='Jogar', cor_texto = PRETO)
+        self.botaoSair = Botao(x=80, y=320, largura=200, altura=50, cor_botao=BRANCO, fonte=self.fonte, texto='Sair', cor_texto = PRETO)
+        self.imagem_fundo = pygame.image.load('app/assets/gradiente_verde_azul.png')
+        self.imagem_fundo = pygame.transform.scale(self.imagem_fundo, (LARGURA_TELA, ALTURA_TELA))
 
     def capturar_eventos(self):
         """Captura eventos do teclado."""
@@ -22,6 +25,7 @@ class Menu:
 
     def desenhar(self):
         #Desenha o menu na tela
-        self.game.tela.fill(PRETO)
-        self.botao.desenhar(self.game.tela)
+        self.game.tela.blit(self.imagem_fundo,(0,0))
+        self.botaoJogar.desenhar(self.game.tela)
+        self.botaoSair.desenhar(self.game.tela)
         
